f52903be0b253d2a5fb2759c580d6c58706e667f
Eason Chai <echai2905@gmail.com>
2024-11-16 18:05:15 +0700

    feat: Add base modules and docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { Web3Module } from './web3/web3.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaModule, Web3Module],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/web3/web3.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { Web3Service } from './web3.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [Web3Service],
+})
+export class Web3Module {}
