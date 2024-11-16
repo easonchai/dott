a9126d085a8ff57d755357d0e4d7283d7a28e24a
krvvs <141290516+krvvs@users.noreply.github.com>
2024-11-17 00:12:41 +0700

    Modified NFT card component

--- miniapp/src/components/NftCard/NftCard.tsx
+++ miniapp/src/components/NftCard/NftCard.tsx
@@ -11,9 +11,11 @@ function NftCard() {
           alt='item'
         />
       </div>
+
       <div className='description'>
         <div className='item-name'>Item Name</div>
         <div className='item-description'>Score x2 for 30 days.</div>
+        <div className='item-price'>2.99 USDC</div>
       </div>
     </div>
   );
